e81e8a868a603bdb9ec7cc55d8573fc193d68b34
Nick Sheward <nickshwrd@gmail.com>
2023-08-21 11:30:50 +0100

    Added post route with JWT

--- app.js
+++ app.js
@@ -7,4 +7,23 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Welcome to the API' });
 });
 
+app.post('/api/posts', (req, res) => {
+  res.json({ message: 'Post created' });
+});
+
+app.post('/api/login', (req, res) => {
+  // Mock user
+  const user = {
+    id: 1,
+    username: 'Barry',
+    email: 'barry@barry.com',
+  };
+  // async approach
+  jwt.sign({ user }, 'secret key', (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+
 app.listen(5000, () => console.log('Server listening on port 5000'));
