4e5bb01af59bdf947b18f801acd95e26694bb96e
Nick Sheward <nickshwrd@gmail.com>
2023-08-21 11:54:04 +0100

    Protected route providing response with token

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Welcome to the API' });
 });
 
-app.post('/api/posts', (req, res) => {
-  res.json({ message: 'Post created' });
+app.post('/api/posts', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secret key', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({ message: 'Post created', authData });
+    }
+  });
 });
 
 app.post('/api/login', (req, res) => {
@@ -25,5 +31,25 @@ app.post('/api/login', (req, res) => {
     });
   });
 });
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers.authorization;
+  // Check if not undefined
+  if (typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next
+    next();
+  } else {
+    // Forbidden, 403 status is not authorised
+    res.sendStatus(403);
+  }
+}
 
 app.listen(5000, () => console.log('Server listening on port 5000'));
