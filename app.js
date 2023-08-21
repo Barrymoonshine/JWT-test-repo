8b9f5d85880f20d9fedff210eb56028f3ab7f164
Nick Sheward <nickshwrd@gmail.com>
2023-08-21 10:49:44 +0100

    Set up app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const express = require(express);
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Welcome to the API' });
+});
+
+app.listen(5000, () => console.log('Server listening on port 5000'));
